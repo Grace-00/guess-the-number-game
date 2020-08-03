8c1ec1360ce87f03cb17fdb4a2a74b9c330ab769
GraziaPalombella <GraziaPalombella@users.noreply.github.com>
2020-08-03 17:25:02 +0200

    Storing variables and initialising count

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //Computer stores random number between 0 and 30 before game starts
 let randomNumber = Math.floor(Math.random() * 30) + 1
+
+//Storing HTML elements into variables to easily use them later + initialising variable for count
+let result = document.querySelector(".result")
+let attempt = document.getElementById("attempt")
+let button = document.getElementById("button")
+let guess = document.getElementById("guess")
+let count = 1
